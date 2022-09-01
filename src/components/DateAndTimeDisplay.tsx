d4498fd882237736cd457e43dfdee6dc36e61731
18EOR <42158152+18EOR@users.noreply.github.com>
2022-09-01 18:11:15 +0100

    added current date and time functionality

--- src/components/DateAndTimeDisplay.tsx
+++ src/components/DateAndTimeDisplay.tsx
@@ -2,17 +2,34 @@ import { useEffect } from 'react';
 import styles from '../styles/appHeaderStyles.module.scss';
 
 function DateAndTimeDisplay() {
+  const d = new Date();
   useEffect(() => {
     // console.log(Date.);
   }, []);
 
+  const getDayOfWeek = (num: number) => {
+    const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return daysOfTheWeek[num];
+  };
+
+  const getMonthOfTheYear = (num: number) => {
+    const monthOfTheYear = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    return monthOfTheYear[num];
+  };
+
+  const formatDate = (num: number) => {
+    return num > 9 ? num : '0' + num;
+  };
+
   return (
     <div>
       <div className={styles.dateTimeWrapper}>
-        <div className={styles.dayNum}>23</div>
+        <div className={styles.dayNum}>{formatDate(d.getDate())}</div>
         <div className={styles.dayMonthYearWrapper}>
-          <div className={styles.day}>Tuesday</div>
-          <div className={styles.monthYear}>Aug, 2022</div>
+          <div className={styles.day}>{getDayOfWeek(d.getDay())}</div>
+          <div className={styles.monthYear}>
+            {getMonthOfTheYear(d.getMonth())}, {d.getFullYear()}
+          </div>
         </div>
       </div>
     </div>
