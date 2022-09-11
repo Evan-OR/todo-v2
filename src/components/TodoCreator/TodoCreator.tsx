7cf4a05563f638e5df1e133abc5960a0b007875a
18EOR <42158152+18EOR@users.noreply.github.com>
2022-09-11 18:59:25 +0100

    Extracted form close button into its own component

--- src/components/TodoCreator/TodoCreator.tsx
+++ src/components/TodoCreator/TodoCreator.tsx
@@ -6,6 +6,7 @@ import IconChooser from './IconChooser';
 import PrioritySelector from './PrioritySelector';
 import { Priority } from '../../utils';
 import BottomFade from './BottomFade';
+import CloseFormButton from './CloseFormButton';
 
 type TodoCreatorProps = {
   show: boolean;
@@ -54,17 +55,6 @@ function TodoCreator(props: TodoCreatorProps) {
     <div className={`${stlyes.mainWrapper} ${show ? '' : stlyes.hide}`}>
       <div className={stlyes.modalWrapper}>
         <form ref={form} className={stlyes.form}>
-          <div className={stlyes.closeButton}>
-            <svg
-              onClick={() => {
-                toggleToDoCreator();
-              }}
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-            </svg>
-          </div>
           <div className={stlyes.title}>Title</div>
           <input
             ref={title}
@@ -99,6 +89,7 @@ function TodoCreator(props: TodoCreatorProps) {
           </div>
         </form>
         <BottomFade />
+        <CloseFormButton toggleToDoCreator={toggleToDoCreator} />
       </div>
     </div>
   );
