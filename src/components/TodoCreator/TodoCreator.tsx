621daa6539e5c0278ae5bf2c3c09a906a908a91d
18EOR <42158152+18EOR@users.noreply.github.com>
2022-09-11 15:41:15 +0100

    Removed Date and Time Options from form

--- src/components/TodoCreator/TodoCreator.tsx
+++ src/components/TodoCreator/TodoCreator.tsx
@@ -18,7 +18,6 @@ function TodoCreator(props: TodoCreatorProps) {
   let [descriptionValue, setDescriptionValue] = useState<string>('');
   let [iconId, setIconId] = useState<number>(0);
   let [colour, setColour] = useState<string>('#ececec');
-  let [dueDate, setDueDate] = useState();
   let [priority, setPriority] = useState<Priority>('Low');
 
   const form = useRef<HTMLFormElement>(null);
@@ -46,10 +45,6 @@ function TodoCreator(props: TodoCreatorProps) {
     setColour(c);
   };
 
-  const handleDueDate = (c: any) => {
-    setDueDate(c);
-  };
-
   const handlePrioritySelector = (e: any) => {
     setPriority(e.target.value);
   };
@@ -58,16 +53,17 @@ function TodoCreator(props: TodoCreatorProps) {
     <div className={`${stlyes.mainWrapper} ${show ? '' : stlyes.hide}`}>
       <div className={stlyes.modalWrapper}>
         <form ref={form} className={stlyes.form}>
-          <svg
-            onClick={() => {
-              toggleToDoCreator();
-            }}
-            className={stlyes.closeButton}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-          >
-            <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-          </svg>
+          <div className={stlyes.closeButton}>
+            <svg
+              onClick={() => {
+                toggleToDoCreator();
+              }}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+            </svg>
+          </div>
           <div className={stlyes.title}>Title</div>
           <input
             ref={title}
@@ -93,9 +89,6 @@ function TodoCreator(props: TodoCreatorProps) {
 
           <div className={stlyes.title}>Priority</div>
           <PrioritySelector handlePrioritySelector={handlePrioritySelector} priority={priority} />
-
-          <div className={stlyes.title}>Due Date {'&'} Time</div>
-          <input onChange={handleDueDate} type="datetime-local"></input>
 
           <div className={`${ButtonStlyes.addBtn} ${stlyes.submitButton}`}>
             <div>Submit</div>{' '}
