2e93daf50d838a785863a209ee40dba791685faa
18EOR <42158152+18EOR@users.noreply.github.com>
2022-09-27 21:59:33 +0100

    added close button to Completed tasks menu

--- src/components/NavBar UI/CompletedTodosModal.tsx
+++ src/components/NavBar UI/CompletedTodosModal.tsx
@@ -1,5 +1,6 @@
 import modalStyles from '../../styles/todoCreatorStyles.module.scss';
 import styles from '../../styles/completedTodosMenuStyles.module.scss';
+import closeButton from '../../styles/todoCreatorStyles.module.scss';
 import { assertIsNode, ToDo } from '../../utils';
 import { useEffect, useRef } from 'react';
 import CompletedTodosDisplay from './CompletedTodosDisplay';
@@ -39,6 +40,15 @@ function CompletedTodosModal(props: CompletedTodosModalProps) {
           ) : (
             <div className={styles.mes}>You have no completed tasks</div>
           )}
+        </div>
+
+        {/* close button */}
+        <div className={closeButton.closeButtonWrapper}>
+          <div className={closeButton.closeIcon}>
+            <svg onClick={() => toggleCompletedTodoView()} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
