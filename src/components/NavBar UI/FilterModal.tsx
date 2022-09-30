e89446ad76c40b6e069c9293a6507be285dcae87
18EOR <42158152+18EOR@users.noreply.github.com>
2022-09-30 23:33:21 +0100

    Adde close button to filter modal

--- src/components/NavBar UI/FilterModal.tsx
+++ src/components/NavBar UI/FilterModal.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import modalStyles from '../../styles/todoCreatorStyles.module.scss';
+import closeButton from '../../styles/todoCreatorStyles.module.scss';
 import { assertIsNode, ToDo } from '../../utils';
 
 type FilterModalProps = {
@@ -25,7 +26,15 @@ function FilterModal(props: FilterModalProps) {
 
   return (
     <div className={modalStyles.mainWrapper}>
-      <div ref={modal} className={modalStyles.modalWrapper}></div>
+      <div ref={modal} className={modalStyles.modalWrapper}>
+        <div className={closeButton.closeButtonWrapper}>
+          <div className={closeButton.closeIcon}>
+            <svg onClick={() => toggleFilterModal()} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+            </svg>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
